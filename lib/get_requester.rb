fe7c27777221c50416fe2650d5d0ebe0b3fd8bc7
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 14:32:30 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,12 +1,11 @@
 require "open-uri"
 require "net/http"
+require "json"
 
 class GetRequester
   
-  def initialize
-    
-    URL =  https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
-  end
+
+URL =  https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
   
   def get_response_body
     
