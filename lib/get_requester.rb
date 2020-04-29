f472342393c0e988ea39525e4420c004af012957
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 14:34:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,7 +9,7 @@ URL =  https://learn-co-curriculum.github.io/json-site-example/endpoints/people.
   
   def get_response_body
     uri = URL.parse(URL)
-    response = NET::HTTP
+    response = NET::HTTP.get_response.
   end
   
   
