6f315b255df724bb25cdf12284245c4a513e8b54
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 14:52:19 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,10 +4,10 @@ require "json"
 
 class GetRequester
   
-  url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  
   
   def initialize(url)
-    
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   end
   
   # def get_response_body
