9e858c17ccdfab48518d3ae1778c9bf9f2610239
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 14:43:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,10 +7,14 @@ class GetRequester
 
 URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
-  uri = URI.parse(URL)
-  response = Net::HTTP.get_response(uri)
-  response.body
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
   
-  
+  def parse_json
+    get_response_body
+  end
   
 end
